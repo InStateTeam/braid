fd1bec8881b52ae28aff6b2b2850ffcedb848be7
Chris Jones <chris.jones@bluebank.io>
2017-10-09 09:16:05 +0100

    styling, and touching up the todo app

--- client/app/components/TodoItem/TodoItem.js
+++ client/app/components/TodoItem/TodoItem.js
@@ -6,18 +6,23 @@ function TodoItem(props) {
 
   return(
     <li className='todo-item-component'>
-      <input
-        type="checkbox" 
-        ref={function(node) {
-              checked = node;              
-            }} 
-        onChange={function(e){ props.complete(props.todo.id); }} />
+      <div className="tick">
+        <input
+          id="done"
+          type="checkbox" 
+          ref={function(node) {
+                checked = node;              
+              }} 
+          onChange={function(e){ props.complete(props.todo.id); }} />
+        <label htmlFor="done"></label> 
+       </div>
+        <span className="index">{props.index}.</span>
         <span>
           {props.todo.text}
         </span>
         <button 
           onClick={function(e){props.remove(props.todo.id)}} >
-          &times;
+          <img src="svg/cross.svg" alt="cross" />
         </button>
     </li>
   );
--- client/app/components/TodoList/TodoList.js
+++ client/app/components/TodoList/TodoList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import TodoItem from 'components/TodoItem/TodoItem';
 
 function TodoList(props) {
-  var todoNode = props.todos.map(function(todo) {
+  var todoNode = props.todos.map(function(todo, index) {
     return(
       <TodoItem 
         todo={todo}
         key={todo.id}
         complete={props.complete}
-        remove={props.remove} />
+        remove={props.remove}
+        index={index + 1} />
      );
   });
 
