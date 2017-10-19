58fe2e37952a0b36699b2e53e13eae0210daa54d
fuzz <fuzz@bluebank.io>
2017-10-19 12:10:04 +0100

    added AuthenticatedSockJSTest.kt - standalone to demonstrate authenticated flow

--- server/src/test/kotlin/io/bluebank/jsonrpc/server/AuthenticatedSockJSTest.kt
+++ server/src/test/kotlin/io/bluebank/jsonrpc/server/AuthenticatedSockJSTest.kt
@@ -2,33 +2,84 @@ package io.bluebank.jsonrpc.server
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.Vertx
 import io.vertx.ext.auth.shiro.ShiroAuth
 import io.vertx.ext.auth.shiro.ShiroAuthOptions
 import io.vertx.ext.auth.shiro.ShiroAuthRealmType
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.BasicAuthHandler
-import io.vertx.ext.web.handler.CookieHandler
-import io.vertx.ext.web.handler.SessionHandler
-import io.vertx.ext.web.handler.UserSessionHandler
+import io.vertx.ext.web.handler.*
+import io.vertx.ext.web.handler.sockjs.SockJSHandler
 import io.vertx.ext.web.sstore.LocalSessionStore
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
+import io.vertx.kotlin.ext.web.handler.sockjs.BridgeOptions
+import io.vertx.kotlin.ext.web.handler.sockjs.PermittedOptions
+import java.text.SimpleDateFormat
+import java.util.*
 
+/**
+ * Not an automated test.
+ * Demonstrates the principles of a secure eventbus over sockjs
+ */
 class AuthenticatedSockJSTest : AbstractVerticle() {
-  override fun start(startFuture: Future<Void>?) {
-    val config = json {
-      obj("properties_path" to "classpath:login/shiro.properties")
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      Vertx.vertx().deployVerticle(AuthenticatedSockJSTest())
     }
-    val provider = ShiroAuth.create(vertx, ShiroAuthOptions().setConfig(config).setType(ShiroAuthRealmType.PROPERTIES))
+
+    private val logger = loggerFor<AuthenticatedSockJSTest>()
+    private val timeFormat = SimpleDateFormat("HH:mm:ss")
+  }
+
+  override fun start(startFuture: Future<Void>) {
     val router = Router.router(vertx)
 
     router.route().handler(CookieHandler.create())
     router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)))
-    router.route().handler(UserSessionHandler.create(provider))
-    router.route("/api/services/*").handler(BasicAuthHandler.create(provider))
+    setupAuth(router)
+    setupEventbusBridge(router)
+    setupTimeService()
+    setupStatic(router)
+
+    val PORT = 8080
     vertx.createHttpServer()
       .requestHandler(router::accept)
-      .listen(80)
-    super.start(startFuture)
+      .listen(PORT) {
+        if (it.succeeded()) {
+          logger.info("started on http://localhost:$PORT")
+        } else {
+          logger.error("failed to startup", it.cause())
+        }
+        startFuture.completer().handle(it.mapEmpty<Void>())
+      }
+
+  }
+
+  private fun setupTimeService() {
+    vertx.setPeriodic(1000) {
+      vertx.eventBus().publish("time", timeFormat.format(Date()))
+    }
+  }
+
+  private fun setupStatic(router: Router) {
+    router.get().handler(StaticHandler.create("eventbus-test").setCachingEnabled(false).setCacheEntryTimeout(1).setMaxCacheSize(1))
+  }
+
+  private fun setupEventbusBridge(router: Router) {
+    val sockJSHandler = SockJSHandler.create(vertx)
+    val outboundPermitted = PermittedOptions(address = "time")
+    val options = BridgeOptions(outboundPermitted = listOf(outboundPermitted))
+    sockJSHandler.bridge(options)
+    router.route("/eventbus/*").handler(sockJSHandler)
+  }
+
+  private fun setupAuth(router: Router) {
+    val config = json {
+      obj("properties_path" to "classpath:login/shiro.properties")
+    }
+    val provider = ShiroAuth.create(vertx, ShiroAuthOptions().setConfig(config).setType(ShiroAuthRealmType.PROPERTIES))
+    router.route().handler(UserSessionHandler.create(provider))
+    router.route("/eventbus/*").handler(BasicAuthHandler.create(provider))
   }
 }
