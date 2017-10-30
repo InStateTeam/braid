6688d17776972b700380b016e9e1b0be2e93f5e1
fuzz <farzad.pezeshkpour@bluebank.io>
2017-10-30 17:55:26 +0000

    updated with new endpoint name as one url slightly wider input

--- server/src/editor/app/scripts/endPoint.js
+++ server/src/editor/app/scripts/endPoint.js
@@ -1,7 +1,9 @@
-export default function getEndPoint(serviceName){
-  const host = window.location.host;
-  const service = serviceName;
-  document.querySelector('#host').innerHTML = "http://" + host;
-  document.querySelector('#calls').value = "/api/jsonrpc/" + service;
-
+export default function getEndPoint(serviceName) {
+  const control = document.querySelector('#calls');
+  if (!serviceName || serviceName.trim() === "") {
+    control.value = ""
+  } else {
+    const host = window.location.host;
+    control.value = window.location.protocol + "//" + host + "/api/jsonrpc/" + serviceName;
+  }
 }
