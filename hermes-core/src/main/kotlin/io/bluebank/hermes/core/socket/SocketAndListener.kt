290798ccb8df26018ea40163d86549a236d657f7
fuzz <farzad.pezeshkpour@bluebank.io>
2017-11-03 14:36:52 +0000

    renamed SocketAndListener to SocketProcessor

--- hermes-core/src/main/kotlin/io/bluebank/hermes/core/socket/SocketAndListener.kt
+++
@@ -1,9 +0,0 @@
-package io.bluebank.hermes.core.socket
-
-/**
- * Classes that implement this interface act both as a socket
- * and a listener to another socket. They are in effect intermediaries
- * acting as converters, or gates in the socket pipeline
- */
-interface SocketAndListener<R1, S1, R2, S2> : Socket<R1, S1>, SocketListener<R2, S2>
-
