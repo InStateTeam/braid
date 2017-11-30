3a025233f7eb3de77d7761da23cd6c52fdd73a96
fuzz <farzad.pezeshkpour@bluebank.io>
2017-11-30 15:54:18 +0000

    refactored to private methods and fields

--- hermes-client-js/hermes-service-proxy.js
+++ hermes-client-js/hermes-service-proxy.js
@@ -2,113 +2,145 @@
 
 const JsonRPC = require('./hermes-json-rpc-client');
 
+/**
+ * Provides a dynamic proxy that intercepts all method calls and forwards them to a hermes JSONRpc service
+ * Handles client use-cases for Promise and Observable style calls
+ */
 class HermesServiceProxy {
   constructor(url, onOpen, onClose, onError, options) {
+    const that = this;
     if (!options) {
       options = {}
     }
     options.noCredentials = true;
-    this.url = url;
-    this.client = new JsonRPC(url, options);
-    this.client.onOpen = onOpen;
-    this.client.onClose = onClose;
-    this.client.onError = onError;
-  }
+    let client = new JsonRPC(url, options);
+    client.onOpen = onOpen;
+    client.onClose = onClose;
+    client.onError = onError;
 
-  uri() {
-    const uri = document.createElement('a');
-    uri.href = this.url;
-    const base = "http://" + uri.hostname + ":" + uri.port;
-    const serviceName = uri.pathname.split("/").filter(i => i.length > 0).pop();
-    if (serviceName !== undefined && serviceName !== null) {
-      return base + "/?service=" + serviceName
-    } else {
-      return base;
+    // --- PRIVATE FUNCTIONS ---
+
+    function bindCallbacks(args) {
+      if (!args) return null;
+      const last3Fns = args.slice(-3).filter(item => {
+        return typeof item === 'function'
+      });
+      if (last3Fns.length === 0) return null;
+      return {
+        onNext: last3Fns[0],
+        onError: last3Fns[1],
+        onCompleted: last3Fns[2]
+      }
     }
-  }
 
-  invoke(method, args) {
-    const callbacks = HermesServiceProxy.bindCallbacks(args);
-    if (callbacks) {
-      return this.invokeForStream(method, args, callbacks);
-    } else {
-      return this.invokeForPromise(method, args);
+    function invokeForPromise(method, args) {
+      args = massageArgs(args);
+      return client.invoke(method, args).then(result => result, onErrorTrap);
     }
-  }
 
-  invokeForPromise(method, args) {
-    args = HermesServiceProxy.massageArgs(args);
-    const thisObj = this;
-    return this.client.invoke(method, args).then(result => result, err => {
-      thisObj.onErrorTrap(err)
-    });
-  }
+    function invokeForStream(method, args, callbacks) {
+      const noFunctionArgs = massageArgs(args.filter(item => {
+        return typeof item !== 'function'
+      }));
+      return client.invokeForStream(method, noFunctionArgs,
+        callbacks.onNext,
+        onErrorWrapper(callbacks.onError),
+        callbacks.onCompleted);
+    }
 
-  invokeForStream(method, args, callbacks) {
-    const noFunctionArgs = HermesServiceProxy.massageArgs(args.filter(item => {
-      return typeof item !== 'function'
-    }));
-    return this.client.invokeForStream(method, noFunctionArgs,
-      callbacks.onNext,
-      this.onErrorWrapper(callbacks.onError),
-      callbacks.onCompleted);
-  }
+    function massageArgs(args) {
+      if (args) {
+        if (args.length === 0) {
+          args = null;
+        } else if (args.length === 1) {
+          args = args[0];
+        }
+      }
+      return args;
+    }
 
-  onErrorWrapper(onError) {
-    const thisObj = this;
-    return (err) => {
-      try {
-        thisObj.onErrorTrap(err)
-      } catch(err2) {
-        if (onError) {
-          onError(err2);
-        } else {
-          console.error(err2);
+    function onErrorWrapper(onError) {
+      return (err) => {
+        try {
+          onErrorTrap(err)
+        } catch(err2) {
+          if (onError) {
+            onError(err2);
+          } else {
+            console.error(err2);
+          }
         }
       }
     }
-  }
 
-  onErrorTrap(err) {
-    console.log(err);
-    if (err.jsonRPCError && err.jsonRPCError.code === -32601) {
-      console.log("%cHermes: %c" + err.message + "\n%cCreate a stub here: " + this.uri(),
-        "font-family: sans-serif; font-size: 14px; font-weight: bold;",
-        "",
-        "font-weight: bold;");
-      throw Error(err.message)
-    } else {
-      throw err;
-    }
-  }
-
-  static massageArgs(args) {
-    if (args) {
-      if (args.length === 0) {
-        args = null;
-      } else if (args.length === 1) {
-        args = args[0];
+    function onErrorTrap(err) {
+      console.log(err);
+      if (err.jsonRPCError && err.jsonRPCError.code === -32601) {
+        console.log("%cHermes: %c" + err.message + "\n%cCreate a stub here: " + uri(),
+          "font-family: sans-serif; font-size: 14px; font-weight: bold;",
+          "",
+          "font-weight: bold;");
+        throw Error(err.message)
+      } else {
+        throw err;
       }
     }
-    return args;
-  }
 
-  static bindCallbacks(args) {
-    if (!args) return null;
-    const last3Fns = args.slice(-3).filter(item => {
-      return typeof item === 'function'
-    });
-    if (last3Fns.length === 0) return null;
-    return {
-      onNext: last3Fns[0],
-      onError: last3Fns[1],
-      onCompleted: last3Fns[2]
+    function uri() {
+      // parse url
+      const uri = document.createElement('a');
+      uri.href = url;
+      const base = "http://" + uri.hostname + ":" + uri.port;
+      const serviceName = uri.pathname.split("/").filter(i => i.length > 0).pop();
+      if (serviceName !== undefined && serviceName !== null) {
+        return base + "/?service=" + serviceName
+      } else {
+        return base;
+      }
     }
+
+    // --- PUBLIC FUNCTIONS ---
+
+    /**
+     * Invoke a method on the bound service with arguments. If the arguments do not end with a set of functions, the
+     * method returns a Promise. Optionally the client may pass a set of functions (at least one), to receive Observable
+     * style notifications.
+     *
+     * A method on the server side is free to return any of the following:
+     * - A single value (primitive or object)
+     * - A Future/Promise/Single object representing the monad on a potentially asynchronous operation
+     * - An Observable representing a monad on a potentially asynchronous operation return a potentially unbounded stream of results
+     *
+     * @param method - the method name on the service to call
+     * @param args - the arguments to be passed. The list of arguments can provide one or more of the following ordered callback functions
+     * @param args[onNext(item)] - callback per item of result returned from the method. The 'item' parameter will hold the respective value object.
+     * @param args[onError(error)] - callback if the stream of results has terminated with an error condition. The 'error' parameter will hold the respective error object.
+     * @param args[onCompleted()] - a callback when the stream has successfully terminated.
+     *
+     */
+    that.invoke = function(method, args) {
+      const callbacks = bindCallbacks(args);
+      if (callbacks) {
+        return invokeForStream(method, args, callbacks);
+      } else {
+        return invokeForPromise(method, args);
+      }
+    }
+
   }
 }
 
-function createProxy(url, onOpen, onClose, options) {
-  return new Proxy(new HermesServiceProxy(url, onOpen, onClose, options), {
+/**
+ * Factory function to create a javascript proxy that dispatches ot a HermesServiceProxy object
+ * @param url - the url to bind to a service. e.g. https://localhost:8080/api/jsonrpc/myservice
+ * @param onOpen - a callback when connection to the service has been successfully made
+ * @param onClose - a callback when the connection to service has been closed
+ * @param onError - a callback when the connection the service has failed
+ * @param options - these are options to control the SockJS transport - see https://github.com/sockjs/sockjs-client
+ * @returns {Proxy}
+ */
+function createProxy(url, onOpen, onClose, onError, options) {
+  return new Proxy(new HermesServiceProxy(url, onOpen, onClose, onError, options), {
     get: (target, propKey) => {
       return function (...args) {
         return target.invoke(propKey, args)
--- hermes-corda-client-js/hermes-corda-proxy.js
+++ hermes-corda-client-js/hermes-corda-proxy.js
@@ -7,75 +7,76 @@ class CordaProxy {
     if (!config.url) {
       throw "config must include url property e.g. https://localhost:8080"
     }
-    const thisObj = this;
-    thisObj.__errors = 0;
-    thisObj.__connections = 0;
-    thisObj.__onOpen = onOpen;
-    thisObj.__onClose = onClose;
-    thisObj.__onError = onError;
-    thisObj.__config = config;
-    thisObj.__options = options;
-    thisObj._bootstrap();
-  }
+    const that = this;
+    let errors = 0;
+    let connections = 0;
+    let requiredConnections = 0;
 
-  onOpen() {
-    if (++this.__connections === this.__requiredConnections && this.__errors === 0 && this.__onOpen) {
-      this.__onOpen()
-    }
-  }
+    // --- PRIVATE FUNCTIONS ---
 
-  onClose() {
-    if (--this.__connections <= 0 && this.__errors === 0 && this.__onClose) {
-      this.__onClose()
-    }
-  }
-
-  onError() {
-    if (++this.__errors === 1 && this.__onError) {
-      this.__onError()
-    }
-  }
-
-  _bootstrap() {
-    const thisObj = this;
-    const url = thisObj.__config.url + "doc/";
-    const oReq = new XMLHttpRequest();
-    oReq.addEventListener('load', () => {
-      if (oReq.status === 200) {
-        thisObj._bindServices(oReq);
-      } else {
-        thisObj._clearCredentials();
-        thisObj._failed("failed to get metadata at " + url + ": " + oReq.statusText);
+    function onInternalOpen() {
+      if (++connections === requiredConnections && errors === 0 && onOpen) {
+        onOpen()
       }
-    });
-    oReq.addEventListener('error', (e) => thisObj._failed(e));
-    oReq.open('GET', url);
-    oReq.send();
-  }
-
-  _bindServices(oReq) {
-    const thisObj = this;
-    const serviceNames = JSON.parse(oReq.response);
-    thisObj.__requiredConnections = serviceNames.length;
-    for (let idx = 0; idx < serviceNames.length; ++idx) {
-      const serviceName = serviceNames[idx];
-      thisObj[serviceName] = new DynamicProxy(thisObj.__config, serviceName,
-        thisObj.onOpen.bind(thisObj), thisObj.onClose.bind(thisObj), thisObj.onError.bind(thisObj), thisObj.__options);
     }
-    thisObj._clearCredentials()
-  }
 
-  _failed(e) {
-    if (this.__onError) {
-      this.__onError(e)
-    } else {
-      console.error(e)
+    function failed(e) {
+      if (onError) {
+        onError(e)
+      } else {
+        console.error(e)
+      }
     }
+
+    function onInternalClose() {
+      if (connections <= 0 && errors === 0 && onClose) {
+        onClose()
+      }
+    }
+
+    function onInternalError() {
+      if (++errors === 1 && onError) {
+        onError()
+      }
+    }
+
+    function bootstrap() {
+      const url = config.url + "doc/";
+      const oReq = new XMLHttpRequest();
+      oReq.addEventListener('load', () => {
+        if (oReq.status === 200) {
+          bindServices(oReq);
+        } else {
+          clearCredentials();
+          failed("failed to get metadata at " + url + ": " + oReq.statusText);
+        }
+      });
+      oReq.addEventListener('error', failed);
+      oReq.open('GET', url);
+      oReq.send();
+    }
+
+    function bindServices(oReq) {
+      const serviceNames = JSON.parse(oReq.response);
+      requiredConnections = serviceNames.length;
+      for (let idx = 0; idx < serviceNames.length; ++idx) {
+        const serviceName = serviceNames[idx];
+        that[serviceName] = new DynamicProxy(config, serviceName, onInternalOpen, onInternalClose, onInternalError, options);
+      }
+      clearCredentials();
+    }
+
+    function clearCredentials() {
+      config.credentials = null;
+    }
+
+    // --- PUBLIC FUNCTIONS ---
+
+    // --- INITIALISATION ---
+    bootstrap();
   }
 
-  _clearCredentials() {
-    this.__config.credentials = null;
-  }
+
 }
 
 module.exports = CordaProxy;
