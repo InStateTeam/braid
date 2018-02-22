3d67a62cf48b96800ec1c435107be825ecc99ab8
Ben Wyeth <gitlab@inebriati.co.uk>
2018-02-22 13:01:42 +0000

    now have a synchronous bind method for braid client

--- braid-core/src/main/kotlin/io/bluebank/braid/core/async/Async.kt
+++ braid-core/src/main/kotlin/io/bluebank/braid/core/async/Async.kt
@@ -3,9 +3,25 @@ package io.bluebank.braid.core.async
 import io.vertx.core.Future
 import io.vertx.core.Future.future
 import rx.Single
+import java.util.concurrent.CountDownLatch
 
 fun <T: Any> Single<T>.toFuture() : Future<T> {
   val result = future<T>()
   this.subscribe(result::complete, result::fail)
   return result
+}
+
+
+fun <T : Any> Future<T>.getOrThrow(): T {
+  val latch = CountDownLatch(1)
+
+  this.setHandler {
+    latch.countDown()
+  }
+
+  latch.await()
+  if (this.failed()) {
+    throw RuntimeException(this.cause())
+  }
+  return this.result()
 }
