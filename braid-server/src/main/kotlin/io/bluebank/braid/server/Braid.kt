48ebb550777d04f80d7e24a690e4450402a5e990
Robin Morris <rozzamozza@yahoo.co.uk>
2019-08-06 15:03:45 +0100

    simplify

--- braid-server/src/main/kotlin/io/bluebank/braid/server/Braid.kt
+++ braid-server/src/main/kotlin/io/bluebank/braid/server/Braid.kt
@@ -15,16 +15,61 @@
  */
 package io.bluebank.braid.server
 
-import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
-import com.typesafe.config.ConfigFactory
+import io.bluebank.braid.corda.BraidConfig
+import io.bluebank.braid.corda.rest.RestConfig
 import io.bluebank.braid.core.logging.loggerFor
-import io.vertx.core.json.Json
+import io.bluebank.braid.server.rpc.NetworkService
+import io.vertx.core.Future
+import io.vertx.core.http.HttpServerOptions
 import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.utilities.NetworkHostAndPort
 
 
-class Braid
+data class Braid(
+        val port: Int = 8080,
+        val userName: String = "",
+        val password: String = "",
+        val nodeAddress: String = ""
+)       {
+
+    fun withPort(port : Int):Braid = this.copy(port = port)
+
+    fun withUserName(userName : String):Braid = this.copy(userName = userName)
+
+    fun withPassword(password : String):Braid = this.copy(password = password)
+
+    fun withNodeAddress(nodeAddress : String):Braid = this.copy(nodeAddress = nodeAddress)
+
+
+    fun startServer() : Future<String>? {
+        val client = CordaRPCClient(NetworkHostAndPort.parse(nodeAddress))
+        val connection = client.start(userName, password)
+        val rpc = connection.proxy
+
+        log.info("Starting Braid on port:" + port)
+        var result = Future.future<String>();
+
+        BraidConfig()
+                // .withFlow(IssueObligation.Initiator::class)
+                .withPort(port)
+                .withHttpServerOptions(HttpServerOptions().apply { isSsl = false })
+                .withRestConfig(RestConfig()
+                        .withPaths {
+                            group("network") {
+                                get("/network/nodes", NetworkService(rpc)::nodes)
+                                get("/network/notaries", NetworkService(rpc)::notaries)
+                                get("/network/nodes/self", NetworkService(rpc)::nodeInfo)
+                            }
+//                            group("cordapps") {
+//                                post("/cordapps/obligation-cordapp/flows/issue-obligation", this@BraidServer::issueObligation)
+//                            }
+                        })
+                .bootstrapBraid(null, result.completer())
+        return result
+    }
+    
+
+}
 
 private val log = loggerFor<Braid>()
 
@@ -34,17 +79,13 @@ fun main(args: Array<String>) {
     if (args.size != 4) {
         throw IllegalArgumentException("Usage: Braid <node address> <username> <password> <port>")
     }
-    val nodeAddress = NetworkHostAndPort.parse(args[0])
-    val username = args[1]
-    val password = args[2]
 
-    val client = CordaRPCClient(nodeAddress)
-    val connection = client.start(username, password)
-    val cordaRPCOperations = connection.proxy
-
-
-    BraidServer(cordaRPCOperations).bootstrapBraid(Integer.valueOf(args[3]))
-            
+    Braid()
+            .withNodeAddress(args[0])
+            .withUserName(args[1])
+            .withPassword(args[2])
+            .withPort(Integer.valueOf(args[3]))
+            .startServer()
 
     //connection.notifyServerAndClose()
 }
--- braid-server/src/main/kotlin/io/bluebank/braid/server/rpc/NetworkService.kt
+++ braid-server/src/main/kotlin/io/bluebank/braid/server/rpc/NetworkService.kt
@@ -13,16 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.bluebank.braid.server
+package io.bluebank.braid.server.rpc
 
 import io.bluebank.braid.corda.BraidConfig
 import io.bluebank.braid.corda.rest.RestConfig
 import io.bluebank.braid.core.logging.loggerFor
+import io.bluebank.braid.server.Braid
 import io.bluebank.braid.server.domain.SimpleNodeInfo
 import io.bluebank.braid.server.domain.toSimpleNodeInfo
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import io.vertx.core.AsyncResult
 import io.vertx.core.http.HttpServerOptions
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
@@ -31,41 +31,10 @@ import net.corda.core.utilities.NetworkHostAndPort
 import java.util.stream.Collectors.toList
 import javax.ws.rs.QueryParam
 
-class BraidServer(val rpc: CordaRPCOps) {
+class NetworkService(val rpc: CordaRPCOps) {
     companion object {
         private val log = loggerFor<Braid>()
     }
-
-
-    public fun bootstrapBraid(port: Int) {
-        log.info("Starting Braid on port:" + port)
-        createBraidConfig(port).bootstrapBraid(null, io.vertx.core.Handler { handleResult(it) })
-    }
-
-    private fun createBraidConfig(port: Int): BraidConfig {
-        return BraidConfig()
-                // .withFlow(IssueObligation.Initiator::class)
-                .withPort(port)
-                .withHttpServerOptions(HttpServerOptions().apply { isSsl = false })
-                .withRestConfig(RestConfig()
-                        .withPaths {
-                            group("network") {
-                                get("/network/nodes", this@BraidServer::nodes)
-                                get("/network/notaries", this@BraidServer::notaries)
-                                get("/network/nodes/self", this@BraidServer::nodeInfo)
-                            }
-//                            group("cordapps") {
-//                                post("/cordapps/obligation-cordapp/flows/issue-obligation", this@BraidServer::issueObligation)
-//                            }
-                        })
-    }
-
-    private fun handleResult(it: AsyncResult<String>) {
-        if (it.failed()) {
-            log.error("failed to start braid", it.cause())
-        }
-    }
-
 
     @ApiOperation(value = "Retrieves all nodes if neither query parameter is supplied. Otherwise returns a list of one node matching the supplied query parameter.")
     fun nodeInfo(
