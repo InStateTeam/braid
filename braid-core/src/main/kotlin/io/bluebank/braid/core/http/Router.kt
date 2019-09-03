b59cfc5eaa80effb5258eb8e361372f0e19a52e0
Robin Morris <rozzamozza@yahoo.co.uk>
2019-09-03 16:15:08 +0100

    fix up reporting errors

--- braid-core/src/main/kotlin/io/bluebank/braid/core/http/Router.kt
+++ braid-core/src/main/kotlin/io/bluebank/braid/core/http/Router.kt
@@ -71,12 +71,12 @@ var log = loggerFor<Router>()
 
 fun HttpServerResponse.end(error: Throwable) {
   val e = if (error is InvocationTargetException) error.targetException else error
-  log.warn("returning error to client", e)
-  val message = e.message?.replace("\n","")?.replace("\r","") ?: "Undefined error"
+  log.debug("returning error to client", e)
+  val message = e.message ?: "Undefined error"
   this
     .putHeader(CONTENT_TYPE, "$TEXT_PLAIN; charset=utf8")
     .putHeader(CONTENT_LENGTH, message.length.toString())
-    .setStatusMessage(message)
+    .setStatusMessage(message.replace("\n","").replace("\r",""))
     .setStatusCode(500)
     .end(message)
 }
