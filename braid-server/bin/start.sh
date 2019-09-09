fcbd486ba91a715f7683cb693a36e8cd5f4e508f
Robin Morris <rozzamozza@yahoo.co.uk>
2019-09-09 15:57:58 +0100

    fix build and standalone jar

---
+++ braid-server/bin/start.sh
@@ -0,0 +1,2 @@
+
+java -jar ./braid-server.jar localhost:10005 user1 test 8999 https://repo1.maven.org/maven2/net/corda/corda-finance-contracts/4.0/corda-finance-contracts-4.0.jar https://repo1.maven.org/maven2/net/corda/corda-finance-workflows/4.0/corda-finance-workflows-4.0.jar
